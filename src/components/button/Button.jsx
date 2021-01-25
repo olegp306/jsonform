ff31d06a4a249d8319ab131db70f81abeb21c2c3
olegpanyukov <opanukov@robokassa.ru>
2021-01-25 09:49:26 +0300

    Initial styles for button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import s from "./Button.module.scss";
 import cn from 'classnames/bind';
 
-const Button = ({ children, ...rest }) => <button {...rest}>{children || 'Отправить'}</button>;
+const Button = ({ children, ...rest }) => <button className={s.button} {...rest}>{children || 'Отправить'}</button>;
 
 export default Button;
