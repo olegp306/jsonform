ad0f36e29ae9f6962a9e58a7a8502ac58663bfbf
olegpanyukov <opanukov@robokassa.ru>
2021-01-25 08:26:56 +0300

    Hide base arrows for number input. Add border styles

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import s from './Input.module.scss'
-import cn from 'classnames/bind';
+import React from "react";
+import s from "./Input.module.scss";
+import cn from "classnames/bind";
 
-const Input = ({label,className, ...rest}) => {
+const Input = ({ label, className, ...rest }) => {
   return (
     <div className={s.inputWrapper}>
-        <span className={s.label} >{label}</span>
-      <input className={cn(s.input,className) } {...rest}/>
+      <span className={s.label}>{label}</span>
+      <input className={cn(s.input, className)} {...rest} />
     </div>
   );
 };
