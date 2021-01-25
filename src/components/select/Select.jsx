fe487c999fe052caffd01ed7f91edfd9c82ef176
olegpanyukov <opanukov@robokassa.ru>
2021-01-25 09:50:05 +0300

    Add base select implementation

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import s from "./Select.module.scss";
+import cn from "classnames/bind";
 
-const Select = () => {
+const Select = ({ label, name, className, options }) => {
   return (
-    <div>
-      Select
+    <div className={s.selectWrapper}>
+      <span className={s.label}>{label}</span>
+      <select name={name} id={name} className={cn(s.select, className)}>
+        {options &&
+          options.map(option => {
+            const { value, label } = option;
+            return <option value={value}>{label}</option>;
+          })}
+      </select>
     </div>
   );
 };
