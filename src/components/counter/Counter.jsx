d65e3198fe6ffbef7c04b5ba21d41ef57927e4c0
olegpanyukov <opanukov@robokassa.ru>
2021-01-25 15:26:02 +0300

    fix counter styles

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -8,8 +8,8 @@ const Counter = ({classname, ...rest }) => {
     <div className={s.counterWrapper}>
       <Input type="number" className={cn(s.input,s.inputText)} {...rest}/>
         <div className={s.buttonsWrapper}>
-            <div className={cn(s.button,s.plusButton)}>+</div>
-            <div className={cn(s.button,s.minusButton)}>-</div>
+            <div className={cn(s.button,s.plusButton,s.unselectable)}>+</div>
+            <div className={cn(s.button,s.minusButton,s.unselectable)}>-</div>
         </div>
     </div>
   );
